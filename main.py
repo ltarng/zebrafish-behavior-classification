9cdf100a41fb0e969cc744f1ec0fc72576042ee5
L.Tarng <converse0201@gmail.com>
2022-12-20 16:27:13 +0800

    New feature calculate & Random forest analysis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import annotate_training_data
-import calculate_dtw
+import preprocess_calculate
 import data_cleaning
 import svm_model
 
@@ -13,6 +13,7 @@ filter_name = "mean"  # filter option: mean, median, kalman
 def main():
     """ EXECUTION OPTION SETTING """
     ifDoDataCleaning = False
+    ifDoPreprocess = False
     ifDoAnnotate = False
     ifDoAnalysis = True
 
@@ -20,6 +21,10 @@ def main():
     if ifDoDataCleaning:
         ifPlotTraj = False
         data_cleaning.data_cleaning(folder_path, video_name, filter_name, ifPlotTraj)
+
+    if ifDoPreprocess:
+        ifPrintDistResult = False
+        preprocess_calculate.calculate_distance_between_frames(folder_path, video_name, filter_name, ifPrintDistResult)
     
     # Training data annotation step
     if ifDoAnnotate:
@@ -28,9 +33,9 @@ def main():
 
     # Analysis step
     if ifDoAnalysis:
-        ifPrintDTWResult = False
-        calculate_dtw.dtw_main(folder_path, video_name, filter_name, ifPrintDTWResult)
+        preprocess_calculate.calculate_main(folder_path, video_name, filter_name)
         svm_model.svm(folder_path, video_name, filter_name)
+        # svm_model.random_forest(folder_path, video_name, filter_name)
 
 
 if __name__ == '__main__':
