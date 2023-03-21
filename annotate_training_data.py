939b4736b78ac0037226940f6b0eff092f76abbb
L.Tarng <18080789+ltarng@users.noreply.github.com>
2023-03-21 17:12:56 +0800

    remove progress bar in annotate_training data.py

--- annotate_training_data.py
+++ annotate_training_data.py
@@ -15,22 +15,19 @@ def auto_annotate(folder_path, video_name, filter_name):
     df['Behavior'] = 0
 
     # Automatic annotate the trajectory data
-    with IncrementalBar('Progress of Annotation', max=len(anno_df.index)) as bar:
-        for index in range(0, len(anno_df.index)):
-            behavior_type = anno_df['BehaviorType'].iloc[index]
-            start_frame, end_frame = anno_df['StartFrame'].iloc[index], anno_df['EndFrame'].iloc[index]
+    for index in range(0, len(anno_df.index)):
+        behavior_type = anno_df['BehaviorType'].iloc[index]
+        start_frame, end_frame = anno_df['StartFrame'].iloc[index], anno_df['EndFrame'].iloc[index]
 
-            # Make behavior dictionary
-            behavior_dict = {'lost': -1, 'normal': 1, 'display': 2, 'circle': 3, 'chase': 4, 'bite': 5}
+        # Make behavior dictionary
+        behavior_dict = {'lost': -1, 'normal': 1, 'display': 2, 'circle': 3, 'chase': 4, 'bite': 5}
+        
+        # Annotate a behavior
+        if behavior_type in behavior_dict.keys():
+            df['Behavior'].iloc[start_frame:end_frame] = behavior_dict[behavior_type]
+        else:
+            df['Behavior'].iloc[start_frame:end_frame] = 100
             
-            # Annotate a behavior
-            if behavior_type in behavior_dict.keys():
-                df['Behavior'].iloc[start_frame:end_frame] = behavior_dict[behavior_type]
-            else:
-                df['Behavior'].iloc[start_frame:end_frame] = 100
-            
-            bar.next()
-
     # Save the annoatated data
     save_path = folder_path + "training_data/"
     df.to_csv(save_path + video_name + "_" + filter_name + "_filtered" + "_annotated.csv", index = False)
