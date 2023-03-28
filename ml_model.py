744a651bd15613439232a69a73d80a5badca330a
L.Tarng <18080789+ltarng@users.noreply.github.com>
2023-03-28 15:57:27 +0800

    Add kernel selection function for SVC

--- ml_model.py
+++ ml_model.py
@@ -25,8 +25,25 @@ def getFeaturesData(feature, df):
     return X
 
 
+def choose_SVC_kernel_model():
+    kernel_num = input("Choose kernel function - 1:linear, 2:poly, 3:rbf, 4:sigmoid \n")
+    if kernel_num == '1':
+        kernel_name = 'linear'
+    elif kernel_num == '2':
+        kernel_name = 'poly'
+    elif kernel_num == '3':
+        kernel_name = 'rbf'
+    elif kernel_num == '4':
+        kernel_name = 'sigmoid'
+    else:
+        print("Your option is not in the list. Please choose again.")
+        choose_SVC_kernel_model()
+    return kernel_name
+
+
 def plot_confusion_matrix(confusion_matrix, model_name, feature, save_folder):
-    classes = ['bite', 'chase', 'circle', 'display', 'normal']
+    # classes = ['bite', 'chase', 'circle', 'display', 'normal']
+    classes = ['bite', 'chase', 'display', 'normal']
     sns.heatmap(confusion_matrix, square=True, annot=True, cmap='Blues',
                 xticklabels=classes, yticklabels=classes)
     # sns.despine(left=False, right=False, top=False, bottom=False)
@@ -42,16 +59,19 @@ def plot_confusion_matrix(confusion_matrix, model_name, feature, save_folder):
 
 def machine_learning_main(folder_path, video_name, filter_name, model_name, feature):
     # Read preprocessed trajectory data
-    df = pd.read_csv(folder_path + video_name + '_' + filter_name + '_preprocessed_result.csv')
+    # df = pd.read_csv(folder_path + video_name + '_' + filter_name + '_preprocessed_result.csv')
+    df = pd.read_csv(folder_path + video_name + '_' + filter_name + '_preprocessed_result_remove_circle.csv')
 
     # Split data into trainging data and testing data
     X = getFeaturesData(feature, df)
     y = df['BehaviorType']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=99)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=54, stratify=y)
 
     # Select model and training
     if model_name == "SVM":
-        model = SVC()
+        kernel_name = choose_SVC_kernel_model()
+        model_name = model_name + '-' + kernel_name
+        model = SVC(kernel=kernel_name)
     elif model_name == "RandomForest":
         model = RandomForestClassifier(n_estimators=1000)
     else:
@@ -61,6 +81,7 @@ def machine_learning_main(folder_path, video_name, filter_name, model_name, feat
 
     # Show the testing result with confusion matrix
     predictions = model.predict(X_test)
+    print(model_name, feature)
     print(classification_report(y_test, predictions))
     cm = confusion_matrix(y_test, predictions)
 
