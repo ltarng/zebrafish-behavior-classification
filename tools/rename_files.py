4c0b88f46da04e77d7f188404837738da3d2bd1a
ltarng <18080789+ltarng@users.noreply.github.com>
2024-09-25 15:37:31 +0800

    Delete tools directory

--- tools/rename_files.py
+++
@@ -1,6 +0,0 @@
-import os
-os.getcwd()
-video_type = "normal"
-folder = "D:/Research/FMTResult_image/" + video_type+ "/"
-for i, filename in enumerate(os.listdir(folder)):
-    os.rename(folder + filename, folder + video_type + "-" + str(i) + ".png")
